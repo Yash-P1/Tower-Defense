3c6b9383a37b078ccff5e6d21a581a3d4bb760c0
Tatvagna Shah <shahtatvagna@gmail.com>
2020-12-06 07:33:03 -0500

    Zombie Character Added
    - Character added
    - Walking anim
    - Path Selection logic (Enemy.cs)
    - Zombie prefeb update

--- Tower Defense Game/Assets/Scripts/Enemy.cs
+++ Tower Defense Game/Assets/Scripts/Enemy.cs
@@ -6,17 +6,21 @@ public class Enemy : MonoBehaviour
 
     private Transform target;
     private int wavpointIndex = 0;
+    public int random;
+    private int destroyPoint = 25;
 
     void Start()
     {
-        target = WayPoints.points[0];   
+        target = WayPoints.points[0];  
+        random = Random.Range(0,3);
     }
 
     void Update()
     {
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
+        transform.LookAt(target);
+       
         if(Vector3.Distance(transform.position, target.position) <= 0.2f)
         {
             GetNextWayPoint();
@@ -24,13 +28,44 @@ public class Enemy : MonoBehaviour
 
         void GetNextWayPoint()
         {
-            if(wavpointIndex >= WayPoints.points.Length - 1)
+
+            if(wavpointIndex > destroyPoint)//WayPoints.points.Length - 1)
             {
                 Destroy(gameObject);
                 return;
             }
-            wavpointIndex++;
-            target = WayPoints.points[wavpointIndex];
+            if(wavpointIndex == 0)
+            {
+                if(random == 0)
+                {
+                    wavpointIndex = 1;
+                    destroyPoint = 13;
+                    // wavpointIndex++;
+                    // target = WayPoints.points[wavpointIndex];
+                    // Debug.Log(target);
+                    return;
+                }else if(random == 1){
+                    wavpointIndex = 14;
+                    destroyPoint = 19;
+                    // target = WayPoints.points[wavpointIndex];
+                    // Debug.Log(target);
+                    return;
+                }else if(random == 2){
+                    wavpointIndex = 20;
+                    destroyPoint = 25;
+                    return;
+                }
+                // wavpointIndex++;
+                target = WayPoints.points[wavpointIndex];
+                wavpointIndex++;
+                Debug.Log(target);
+            }else{
+                target = WayPoints.points[wavpointIndex];
+                wavpointIndex++;
+                Debug.Log(target); 
+            }
+             
+
         }
 
     }
