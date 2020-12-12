a8e5af18ec8f06ef1d1412aa132f891402cd70ef
Yash-P1 <yspatel97@gmail.com>
2020-12-12 05:17:58 -0500

    Camera movement
    - Added Camera movement

---
+++ Tower Defense Game/Assets/Incomplete Tasks.cs
@@ -0,0 +1,8 @@
+﻿/*
+ *  - CameraController.cs -> Added boundaries to clip the camera while cooming in and out (https://www.youtube.com/watch?v=KkYco_7-ULA)
+ *                        -> Add UI button to toggle rotation for Camera movement
+ * 
+ * 
+ * 
+ * 
+ */
