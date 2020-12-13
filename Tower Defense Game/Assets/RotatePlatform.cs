c050f08b443950057317d338c4a330ad276fbe3e
Yash-P1 <yspatel97@gmail.com>
2020-12-13 17:36:45 -0500

    UI added
    - Added few prefabs for better UI

---
+++ Tower Defense Game/Assets/RotatePlatform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatform : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 20f, 0f) * Time.deltaTime);
+    }
+}
