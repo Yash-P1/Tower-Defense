286d26dcd3635257130e51b09a98794e4831ef4f
Tatvagna Shah <shahtatvagna@gmail.com>
2020-12-14 21:05:51 -0500

    Lazer turret Added

--- Tower Defense Game/Assets/Scripts/Turret.cs
+++ Tower Defense Game/Assets/Scripts/Turret.cs
@@ -1,94 +1,140 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Turret : MonoBehaviour
-{
-    private Transform target;
+public class Turret : MonoBehaviour {
 
-    [Header("Attributes")]
+	private Transform target;
 
-    public float range = 15f;
-    public float fireRate = 1f;
-    private float fireCountdown = 0f;
+	[Header("General")]
 
-    [Header("Unity Setup Fields")]
+	public float range = 15f;
 
-    public string enemyTag = "Enemy";
+	[Header("Use Bullets (default)")]
+	public GameObject bulletPrefab;
+	public float fireRate = 1f;
+	private float fireCountdown = 0f;
 
-    public Transform partToRotate;
-    public float turnSpeed = 10f;
+	[Header("Use Laser")]
+	public bool useLaser = false;
+	public LineRenderer lineRenderer;
+	public ParticleSystem impactEffect;
+	public Light impactLight;
 
-    public GameObject bulletPrefab;
-    public Transform firePoint;
+	[Header("Unity Setup Fields")]
 
+	public string enemyTag = "Enemy";
 
-    void Start()
-    {
-        InvokeRepeating("UpdateTarget", 0f, 0.5f);   
-    }
-    void UpdateTarget()
-    {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
-        float shortestDistance = Mathf.Infinity;
-        GameObject nearestEnemy = null;
+	public Transform partToRotate;
+	public float turnSpeed = 10f;
 
-        foreach(GameObject enemy in enemies)
-        {
-            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-            if(distanceToEnemy < shortestDistance)
-            {
-                shortestDistance = distanceToEnemy;
-                nearestEnemy = enemy;
-            }
-        }
+	public Transform firePoint;
 
-        if(nearestEnemy != null && shortestDistance <= range)
-        {
-            target = nearestEnemy.transform;
-        }
-        else
-        {
-            target = null;
-        }
-    }
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+	
+	void UpdateTarget ()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+		foreach (GameObject enemy in enemies)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+			if (distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
 
-    void Update()
-    {
-        if (target == null)
-            return;
+		if (nearestEnemy != null && shortestDistance <= range)
+		{
+			target = nearestEnemy.transform;
+		} else
+		{
+			target = null;
+		}
 
-        // Target lock
-        Vector3 dir = target.position - transform.position;
-        Quaternion lookRotation = Quaternion.LookRotation(dir);
-        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
-        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
 
-        if(fireCountdown <= 0f)
-        {
-            Shoot();
-            fireCountdown = 1f / fireRate;
-        }
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+		{
+			if (useLaser)
+			{
+				if (lineRenderer.enabled)
+				{
+					lineRenderer.enabled = false;
+					impactEffect.Stop();
+					impactLight.enabled = false;
+				}
+			}
 
-        fireCountdown -= Time.deltaTime;
+			return;
+		}
 
-    }
+		LockOnTarget();
 
-    void Shoot()
-    {
-        GameObject bulletGO =  (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Bullet bullet = bulletGO.GetComponent<Bullet>();
+		if (useLaser)
+		{
+			Laser();
+		} else
+		{
+			if (fireCountdown <= 0f)
+			{
+				Shoot();
+				fireCountdown = 1f / fireRate;
+			}
 
-        if(bullet != null)
-        {
-            bullet.Seek(target);
-        }
-        
-    }
+			fireCountdown -= Time.deltaTime;
+		}
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, range);
-    }
+	}
+
+	void LockOnTarget ()
+	{
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+		partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+	void Laser ()
+	{
+		if (!lineRenderer.enabled)
+		{
+			lineRenderer.enabled = true;
+			impactEffect.Play();
+			impactLight.enabled = true;
+		}
+
+        Vector3 enemyOffset = new Vector3(0,1.5f,0);
+
+		lineRenderer.SetPosition(0, firePoint.position);
+		lineRenderer.SetPosition(1, target.position + enemyOffset);
+
+		Vector3 dir = firePoint.position - target.position;
+
+		impactEffect.transform.position = target.position  + enemyOffset;
+
+		impactEffect.transform.rotation = Quaternion.LookRotation(dir);
+	}
+
+	void Shoot ()
+	{
+		GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+		if (bullet != null)
+			bullet.Seek(target);
+	}
+
+	void OnDrawGizmosSelected ()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, range);
+	}
 }
