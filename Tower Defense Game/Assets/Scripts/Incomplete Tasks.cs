8544dc306eb39fda4e02ba021a8973b6540dba3d
Yash-P1 <yspatel97@gmail.com>
2020-12-17 21:13:31 -0500

    Final Commit

--- Tower Defense Game/Assets/Scripts/Incomplete Tasks.cs
+++
@@ -1,7 +0,0 @@
-﻿/*
- *  - CameraController.cs -> Added boundaries to clip the camera while cooming in and out (https://www.youtube.com/watch?v=KkYco_7-ULA)
- *                        -> Add UI button to toggle rotation for Camera movement
- * 
- *  - BuildMAnager.cs -> Add logic for spawning the turrets in the air 
- * 
- */
